8a2aa1dbe5c1d20184defd91f338d060685b7098
Denis Nikolskiy <den-apple@ya.ru>
2022-06-25 18:28:31 +0200

    refactor: исправить замечания ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,29 @@
 const popupElement = document.querySelector('.popup');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const titleElement = document.querySelector('.profile__title');
+const subtitleElement = document.querySelector('.profile__subtitle');
+const formElement = document.querySelector('.form');
+const nameInput = formElement.querySelector('.form__input_type_name');
+const jobInput = formElement.querySelector('.form__input_type_job');
 
 const openPopup = function() {
   popupElement.classList.add('popup_opened');
+  nameInput.value = titleElement.textContent;
+  jobInput.value = subtitleElement.textContent;
 }
+
 const closePopup = function() {
   popupElement.classList.remove('popup_opened');
 }
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-
-const formElement = document.querySelector('.form');
-const nameInput = formElement.querySelector('.form__name');
-const jobInput = formElement.querySelector('.form__job');
-
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
-  const titleElement = document.querySelector('.profile__title');
-  const subtitleElement = document.querySelector('.profile__subtitle');
   titleElement.textContent = nameInput.value;
   subtitleElement.textContent = jobInput.value;
   closePopup();
  }
 
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
