b5c2c2c2aca8b10f80d9d94f827f5e5af859bdce
Denis Nikolskiy <den-apple@ya.ru>
2022-07-06 22:30:13 +0200

    feat: добавить попап добавления места

--- scripts/index.js
+++ scripts/index.js
@@ -25,56 +25,69 @@ const initialCards = [
   }
 ];
 const popupElement = document.querySelector('.popup');
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const popupEditProfileElement = document.querySelector('.popup_purpose_edit-profile');
+const popupAddPlaceElement = document.querySelector('.popup_purpose_add-place');
+
+const popupEditProfileOpenButtonElement = document.querySelector('.profile__edit-button');
+const popupAddPlaceOpenButtonElement = document.querySelector('.profile__add-button');
+
+const popupEditProfileCloseButtonElement = popupEditProfileElement.querySelector('.popup__close-button_place_edit-profile');
+const popupAddPlaceCloseButtonElement = popupAddPlaceElement.querySelector('.popup__close-button_place_add-place');
+
 const titleElement = document.querySelector('.profile__title');
 const subtitleElement = document.querySelector('.profile__subtitle');
-const formElement = document.querySelector('.form');
-const nameInput = formElement.querySelector('.form__input_type_name');
-const jobInput = formElement.querySelector('.form__input_type_job');
 
-const elementsElement = document.querySelector('.elements');
+const formEditProfileElement = document.querySelector('.form_purpose_edit-profile');
+const nameInput = formEditProfileElement.querySelector('.form__input_type_name');
+const jobInput = formEditProfileElement.querySelector('.form__input_type_job');
 
-const openPopup = function() {
-  popupElement.classList.add('popup_opened');
-  nameInput.value = titleElement.textContent;
-  jobInput.value = subtitleElement.textContent;
-}
+const postsElement = document.querySelector('.elements');
+const postTemplate = document.querySelector('#template-element').content;
 
-const closePopup = function() {
-  popupElement.classList.remove('popup_opened');
-}
-
- const closePopupByClickOnOverlay = function(event) {
-  if (event.target !== event.currentTarget) {
-    return;
+const openPopup = function(element) {
+    element.classList.add('popup_opened');
+    if (element === popupElement) {
+      nameInput.value = titleElement.textContent; 
+      jobInput.value = subtitleElement.textContent;
+    }
   }
-  closePopup();
- }
+
+const closePopup = function(element) {
+  element.classList.remove('popup_opened');
+}
+
+//  const closePopupByClickOnOverlay = function(event) {
+//   if (event.target !== event.currentTarget) {
+//     return;
+//   }
+//   closePopup();
+//  }
 
  function formSubmitHandler (evt) {
   evt.preventDefault(); 
   titleElement.textContent = nameInput.value;
   subtitleElement.textContent = jobInput.value;
-  closePopup();
+  closePopup(popupEditProfileElement);
  }
 
 const fillingElements = function() {
   for (i = 0; i < initialCards.length; i++) {
-    const elementTemplate = document.querySelector('#template-element').content;
-    const articleElement = elementTemplate.querySelector('.element').cloneNode(true);
+
+    const articleElement = postTemplate.querySelector('.element').cloneNode(true);
 
     articleElement.querySelector('.element__image').src = initialCards[i].link;
     articleElement.querySelector('.element__image').alt = initialCards[i].name;
     articleElement.querySelector('.element__title').textContent = initialCards[i].name;
 
-    elementsElement.append(articleElement);
+    postsElement.append(articleElement);
   }
 }
 
 fillingElements();
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
-popupElement.addEventListener('click', closePopupByClickOnOverlay);
+popupEditProfileOpenButtonElement.addEventListener('click', () => openPopup(popupEditProfileElement));
+popupAddPlaceOpenButtonElement.addEventListener('click', () => openPopup(popupAddPlaceElement));
+popupEditProfileCloseButtonElement.addEventListener('click', () => closePopup(popupEditProfileElement));
+popupAddPlaceCloseButtonElement.addEventListener('click',  () => closePopup(popupAddPlaceElement));
+formEditProfileElement.addEventListener('submit', formSubmitHandler);
+// popupElement.addEventListener('click', closePopupByClickOnOverlay);
